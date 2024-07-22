3aec0fe822c3daf2375487f74dfc0e73f9cadc64
Benjamin T. Johnson <benjamin.t.johnson@noaa.gov>
2024-07-22 19:43:16 +0000

    updated Get_CRTM_Binary_Files.sh to point at fix_REL-3.1.1.0.tgz

--- Get_CRTM_Binary_Files.sh
+++ Get_CRTM_Binary_Files.sh
@@ -3,7 +3,7 @@
 # This script is used to manually download the tarball of binary and netcdf coefficient files.
 # The same files also download automatically during the cmake step, so you don't have to actually run this manually. 
 
-foldername="fix_REL-3.1.0.1_01252024"
+foldername="fix_REL-3.1.1.0"
 filename="${foldername}.tgz"
 echo "$filename"
 break
