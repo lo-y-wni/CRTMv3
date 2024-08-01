b4bef18e056d0380d03c5b2e39f83bc961ff905c
Quanhua Liu <Quanhua.Liu@noaa.gov>
2024-08-01 02:02:54 +0000

    Added on 7/31/2024

---
+++ Test_CRTM_Aircraft/ifort_lib.sh
@@ -0,0 +1,2 @@
+#export PATH=/opt/intel/compilers_and_libraries_2017.0.098/linux/bin/intel64:/data/starfs1/libs/netcdf-fortran-4.5.2-ifort/bin:$PATH
+export LD_LIBRARY_PATH=/data/starfs1/libs/netcdf-4.2-ifort/lib:/data/starfs1/libs/hdf5-1.10.6-ifort/lib:$LD_LIBRARY_PATH
